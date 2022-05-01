1410fa1db096afd8c37161918c069130816432ab
Yenny Huang <yennyhuang@macbook-air.myfiosgateway.com>
2022-05-01 15:23:17 -0400

    Added Weekly View

---
+++ app/src/main/java/csc221finalproject/calendarcalorie/myapplication/EventEdit.java
@@ -0,0 +1,14 @@
+package csc221finalproject.calendarcalorie.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EventEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_edit);
+    }
+}
---
+++ app/src/main/java/csc221finalproject/calendarcalorie/myapplication/WeeklyView.java
@@ -0,0 +1,14 @@
+package csc221finalproject.calendarcalorie.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WeeklyView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_view);
+    }
+}
